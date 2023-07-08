02a86c2a03857b06a58606d8b49eee390cc3441d
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-08 17:32:02 +0300

    improve list logic

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,15 +1,17 @@
 function ListGroup() {
     const items = ['a', 'b', 'c',]
+    if (items.length === 0) {
+        return <p>No item found</p>
+    }
     return (
         <>
             <h1>List</h1>
-            {
-                items.length === 0 ? <p>No item found</p> : <ul className="list-group">
-                    {items.map(item =>
-                        <li key={item} className="list-group-item">{item}</li>
-                    )}
-                </ul>
-            }
+            { items.length === 0 ? <p>No item found</p> : null }
+            <ul className="list-group">
+                {items.map(item =>
+                    <li key={item} className="list-group-item">{item}</li>
+                )}
+            </ul>
         </>
     )
 }
