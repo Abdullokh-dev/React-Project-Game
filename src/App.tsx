d1e8a4673e786c33f1e7dce337baecb43bda6e91
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-09 22:44:34 +0300

    Updating Array of Object

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import {useState} from "react";
 
 function App() {
-    const [tags, setTags] = useState(['happy', 'cheerful']);
+    const [bugs, setBugs] = useState([
+        {id: 1, title: 'Bug 1', fixed: false},
+        {id: 2, title: 'Bug 2', fixed: false}
+    ]);
 
     const handleClick = () => {
-        //Add
-        setTags([...tags, 'sad'])
-
-        //Remove
-        setTags(tags.filter(tag => tag !== 'happy'))
-
-        //Update
-        setTags(tags.map(tag => tag === 'happy' ? 'happiness' : tag))
+        setBugs(bugs.map(bug => bug.id === 1 ? {...bug, fixed: true} : bug))
     }
     return (
         <>
-            {tags}
             <button onClick={handleClick}>click</button>
         </>
     )
