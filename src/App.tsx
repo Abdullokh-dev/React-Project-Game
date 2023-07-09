7d40a113ad404d8fdf7e075fd47d2894435a8061
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-09 14:01:13 +0300

    Build Button Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
     const items = ['USA', 'Russia', 'Ukraine']
     return(
         <>
             <ListGroup items={items} heading={"Cities"} onSelectItem={'handle'}/>
-            <Alert>
-                Abdulloh
-            </Alert>
+            <Alert>Abdulloh</Alert>
+            <Button onClick={() => console.log('clicked')}> my Button </Button>
         </>
         )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+interface Props {
+    children: string;
+    color?: string;
+    onClick: void;
+}
+
+function Button({children, color = 'primary', onClick}: Props) {
+    return <button type="button" className={'btn btn-' + color} onClick={onClick}> {children}</button>
+}
+
+export default Button;
