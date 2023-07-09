c5bdc0e240321384700780cd5fabf02397d688a0
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-09 14:39:42 +0300

    how structure css components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
+import ListGroup from "./components/ListGroup/";
 import {useState} from "react";
 
 function App() {
     const [alertVisible, setAlertVisibility] = useState(false);
+    const items = ['a', 'a', 'c'];
     return(
         <>
             {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
             <Button color={'danger'} onClick={() => setAlertVisibility(true)}> my Button </Button>
+            <ListGroup items={items} heading={'List'}/>
         </>
         )
 }
