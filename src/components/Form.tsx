b114355cdd9a116d8f9e0230b2fb5de596184fd3
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-10 21:46:37 +0300

    Controlled Components

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,13 @@
-import {FormEvent, useRef} from "react";
+import {FormEvent, useState} from "react";
 
 function Form() {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const ageRef = useRef<HTMLInputElement>(null);
-    const person = {name: '', age: 0}
+    const [person, setPerson] = useState({
+        name: '',
+        age: ''
+    })
 
     const myFunc = (event: FormEvent) => {
         event.preventDefault()
-        if (nameRef !== null)
-            person.name = nameRef.current.value;
-
-        if (ageRef !== null)
-            person.age = parseInt(ageRef.current.value);
 
         console.log(person)
     }
@@ -20,12 +16,27 @@ function Form() {
             <form onSubmit={myFunc}>
                 <div className="mb-3 px-5">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input ref={nameRef} id='name' type="text" className="form-control"/>
+                    <input
+                        onChange={(event) =>
+                           setPerson({...person, name: event.target.value})
+                         }
+                        value={person.name}
+                        id='name'
+                        type="text"
+                        className="form-control"
+                    />
                 </div>
 
                 <div className="mb-3 px-5">
                     <label htmlFor="age" className="form-label">Age</label>
-                    <input ref={ageRef} id='age' type="number" className="form-control"/>
+                    <input
+                        onChange={(event) =>
+                            setPerson({...person, age: event.target.value})
+                        }
+                        value={person.age}
+                        id='age'
+                        type="number"
+                        className="form-control"/>
                 </div>
 
                 <button type={"submit"} className={'btn btn-primary mx-5'}> Submit </button>
