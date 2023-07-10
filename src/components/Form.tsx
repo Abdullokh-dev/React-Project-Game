ee7936272ad185b6ce62c4f1e230cfd876a8ad00
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-10 21:22:40 +0300

    Access input field

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,31 @@
-import {FormEvent} from "react";
+import {FormEvent, useRef} from "react";
 
 function Form() {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = {name: '', age: 0}
+
     const myFunc = (event: FormEvent) => {
         event.preventDefault()
-        console.log('a')
+        if (nameRef !== null)
+            person.name = nameRef.current.value;
+
+        if (ageRef !== null)
+            person.age = parseInt(ageRef.current.value);
+
+        console.log(person)
     }
     return (
         <>
             <form onSubmit={myFunc}>
                 <div className="mb-3 px-5">
                     <label htmlFor="name" className="form-label">Name</label>
-                    <input id='name' type="text" className="form-control"/>
+                    <input ref={nameRef} id='name' type="text" className="form-control"/>
                 </div>
 
                 <div className="mb-3 px-5">
                     <label htmlFor="age" className="form-label">Age</label>
-                    <input id='age' type="number" className="form-control"/>
+                    <input ref={ageRef} id='age' type="number" className="form-control"/>
                 </div>
 
                 <button type={"submit"} className={'btn btn-primary mx-5'}> Submit </button>
