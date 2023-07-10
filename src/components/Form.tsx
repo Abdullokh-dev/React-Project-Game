1e31f80e20335254b99d02a6d1712740ed326e34
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-10 23:25:49 +0300

    Form Validating with formState

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,11 @@
 import {FieldValues, useForm} from "react-hook-form";
 
+interface FormData {
+    name: string,
+    age: number
+}
 function Form() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
     const onSubmit = (data: FieldValues) => console.log(data);
     return (
         <>
@@ -9,11 +13,13 @@ function Form() {
                 <div className="mb-3 px-5">
                     <label htmlFor="name" className="form-label">Name</label>
                     <input
-                        {...register('name')}
+                        {...register('name', {required: true, minLength: 3})}
                         id='name'
                         type="text"
                         className="form-control"
                     />
+                    { errors.name?.type === 'required' && <p className={'text-danger'}>The name field is required</p> }
+                    { errors.name?.type === 'minLength' && <p className={'text-warning'}>The name must be at least 3 characters</p> }
                 </div>
 
                 <div className="mb-3 px-5">
